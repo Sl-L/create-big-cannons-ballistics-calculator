c715fc7499ecb5e2698412e833263dde064e23c1
Sl-L <zeroscsps@gmail.com>
2024-12-17 17:25:50 -0300

    First GUI version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,339 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::{egui, NativeOptions};
+use egui::{ComboBox, Grid, RichText};
+use egui_dock::{DockArea, DockState, NodeIndex, SurfaceIndex};
+
+use regex::Regex;
+
+const NORMAL_TEXT: f32 = 15.0;
+const TITLE_TEXT: f32 = 20.0;
+
+fn main() -> eframe::Result<()> {
+    let options = NativeOptions::default();
+    eframe::run_native(
+        "Create Big Cannons - H's Ballistics Calculator",
+        options,
+        Box::new(|_cc| Ok(Box::<MyApp>::default())),
+    )
+}
+
+pub fn verify_signed_float_input(s: &mut String) {
+    let re = Regex::new(r"^-?[0-9]*\.?[0-9]*").unwrap();
+    let cap = re.captures(s);
+    if cap.is_none() {
+        *s = "".to_string();
+    } else {
+        *s = re.captures(s).unwrap().get(0).unwrap().as_str().to_string();
+    }
+}
+
+pub fn verify_positive_integer_input(s: &mut String) {
+    let re = Regex::new(r"^[1-9]+[0-9]*").unwrap();
+    let cap = re.captures(s);
+    if cap.is_none() {
+        *s = "".to_string();
+    } else {
+        *s = re.captures(s).unwrap().get(0).unwrap().as_str().to_string();
+    }
+}
+
+struct Pair {
+    pub direct_shot: f64,
+    pub indirect_shot: f64
+}
+
+enum MyTabKind {
+    Cartesian,
+}
+
+struct MyTab {
+    kind: MyTabKind,
+    surface: SurfaceIndex,
+    node: NodeIndex,
+    c_x: String,
+    c_y: String,
+    c_z: String,
+    t_x: String,
+    t_y: String,
+    t_z: String,
+    ammo_type: String,
+    charges: String,
+    yaw: f64,
+    pitch: Pair,
+    time: Pair,
+    impact_angle: Pair,
+}
+
+impl MyTab {
+    fn cartesian(surface: SurfaceIndex, node: NodeIndex) -> Self {
+        Self {
+            kind: MyTabKind::Cartesian,
+            surface,
+            node,
+            c_x: "".to_string(),
+            c_y: "".to_string(),
+            c_z: "".to_string(),
+            t_x: "".to_string(),
+            t_y: "".to_string(),
+            t_z: "".to_string(),
+            ammo_type: "".to_string(),
+            charges: "".to_string(),
+            yaw: f64::NAN,
+            pitch: Pair {direct_shot: f64::NAN, indirect_shot: f64::NAN},
+            time: Pair {direct_shot: f64::NAN, indirect_shot: f64::NAN},
+            impact_angle: Pair {direct_shot: f64::NAN, indirect_shot: f64::NAN},
+        }
+    }
+
+    fn cartesian_tab_content(&mut self, ui: &mut egui::Ui) {
+        ui.vertical_centered(|ui| {
+            ui.label(RichText::new("Cartesian").size(30.0));
+        });
+
+        Grid::new("title")
+        .min_col_width(ui.available_width() / 2.0 - 100.0)
+        .max_col_width(ui.available_width() / 2.0 - 100.0)
+        .min_row_height(15.0)
+        .show(ui, |ui| {
+            ui.vertical(|ui| {
+                Grid::new("cannon-info")
+                .min_col_width(10.0)
+                .max_col_width(80.0)
+                .min_row_height(15.0)
+                .show(ui, |ui| {
+                    ui.label("");
+                    ui.label(RichText::new(" Cannon").size(TITLE_TEXT));
+                    ui.end_row();
+
+                    ui.label(RichText::new("X: ").size(NORMAL_TEXT));
+                    if ui.text_edit_singleline(&mut self.c_x).changed() {
+                        verify_signed_float_input(&mut self.c_x);
+                    }
+
+                    ui.end_row();
+                    ui.label(RichText::new("Y: ").size(NORMAL_TEXT));
+                    if ui.text_edit_singleline(&mut self.c_y).changed() {
+                        verify_signed_float_input(&mut self.c_y);
+                    }
+
+                    ui.end_row();
+                    ui.label(RichText::new("Z: ").size(NORMAL_TEXT));
+                    if ui.text_edit_singleline(&mut self.c_z).changed() {
+                        verify_signed_float_input(&mut self.c_z);
+                    }
+                    ui.end_row();
+                    ui.label(RichText::new("  ").size(NORMAL_TEXT));
+                });
+            });
+            ui.vertical(|ui| {
+                Grid::new("target-info")
+                .min_col_width(10.0)
+                .max_col_width(80.0)
+                .show(ui, |ui| {
+                    ui.label("");
+                    ui.label(RichText::new(" Target").size(TITLE_TEXT));
+                    ui.end_row();
+
+                    ui.label(RichText::new("X: ").size(NORMAL_TEXT));
+                    if ui.text_edit_singleline(&mut self.t_x).changed() {
+                        verify_signed_float_input(&mut self.t_x);
+                    }
+
+                    ui.end_row();
+                    ui.label(RichText::new("Y: ").size(NORMAL_TEXT));
+                    if ui.text_edit_singleline(&mut self.t_y).changed() {
+                        verify_signed_float_input(&mut self.t_y);
+                    }
+
+                    ui.end_row();
+                    ui.label(RichText::new("Z: ").size(NORMAL_TEXT));
+                    if ui.text_edit_singleline(&mut self.t_z).changed() {
+                        verify_signed_float_input(&mut self.t_z);
+                    }
+                });
+            });
+        });
+        
+        ui.horizontal(|ui| {
+            ComboBox::new("Ammo type", RichText::new(" :Ammo type").size(NORMAL_TEXT))
+            .selected_text(RichText::new(format!("{}", self.ammo_type)).size(NORMAL_TEXT))
+            .show_ui(ui, |ui| {
+                for ammo_type in ["Normal", "Mortar"] {
+                    ui.selectable_value(
+                        &mut self.ammo_type,
+                        ammo_type.to_string(),
+                        RichText::new(ammo_type).size(NORMAL_TEXT)
+                    );
+                }
+            });
+
+            ui.add_space(10.0);
+
+            Grid::new("0987")
+            .max_col_width(30.0)
+            .show(ui, |ui| {
+                if ui.text_edit_singleline(&mut self.charges).changed() {
+                    verify_positive_integer_input(&mut self.charges);
+                }
+            });
+
+            ui.label(RichText::new(" :Powder charges").size(NORMAL_TEXT));
+
+            match self.charges.parse::<f64>() { //TO-DO: add effect of charges
+                Ok(_) => {}
+                Err(_) => {}
+            };
+        });
+        
+        match self.ammo_type.as_str() { //TO-DO: add types of ammo and properties
+            "Normal" => {}
+            "Mortar" => {}
+            _ => {}
+        }
+
+        if ui.button(RichText::new("Calculate").size(TITLE_TEXT)).clicked() {
+            let mut x: f64 = f64::NAN;
+            let mut y: f64 = f64::NAN;
+            let mut z: f64 = f64::NAN;
+
+            match self.t_x.parse::<f64>() {
+                Ok(t_x) => x += t_x,
+                Err(_) => {}
+            }
+            match self.c_x.parse::<f64>() {
+                Ok(t_x) => x -= t_x,
+                Err(_) => {}
+            }
+
+            match self.t_y.parse::<f64>() {
+                Ok(t_y) => y += t_y,
+                Err(_) => {}
+            }
+            match self.c_y.parse::<f64>() {
+                Ok(t_y) => y -= t_y,
+                Err(_) => {}
+            }
+
+            match self.t_z.parse::<f64>() {
+                Ok(t_z) => z += t_z,
+                Err(_) => {}
+            }
+            match self.c_z.parse::<f64>() {
+                Ok(t_z) => z -= t_z,
+                Err(_) => {}
+            }
+        }
+
+        Grid::new("results")
+        .min_col_width(ui.available_width() / 2.0)
+        .max_col_width(ui.available_width() / 2.0)
+        .show(ui, |ui| {
+            ui.vertical(|ui| {
+                ui.group(|ui| {
+                    ui.label(RichText::new("Direct Shot     ").size(NORMAL_TEXT * (4.0/3.0)));
+                    ui.label(RichText::new(format!("Yaw: {:.4}°", self.yaw)).size(NORMAL_TEXT));
+                    ui.label(RichText::new(format!("Pitch: {}°", self.pitch.direct_shot)).size(NORMAL_TEXT));
+                    ui.label(RichText::new(format!("Flight time: {:.4}s", self.time.direct_shot)).size(NORMAL_TEXT));
+                    ui.label(RichText::new(format!("Impact angle: {:.4}°", self.impact_angle.direct_shot)).size(NORMAL_TEXT));
+                });
+            });
+            ui.vertical(|ui| {
+                ui.group(|ui| {
+                    ui.label(RichText::new("Indirect Shot   ").size(NORMAL_TEXT * (4.0/3.0)));
+                    ui.label(RichText::new(format!("Yaw: {:.4}°", self.yaw)).size(NORMAL_TEXT));
+                    ui.label(RichText::new(format!("Pitch: {}°", self.pitch.indirect_shot)).size(NORMAL_TEXT));
+                    ui.label(RichText::new(format!("Flight time: {:.4}s", self.time.indirect_shot)).size(NORMAL_TEXT));
+                    ui.label(RichText::new(format!("Impact angle: {:.4}°", self.impact_angle.indirect_shot)).size(NORMAL_TEXT));
+                });
+            });
+        });
+    }
+
+    fn title(&self) -> String {
+        match self.kind {
+            MyTabKind::Cartesian => format!("Cartesian Tab {}", self.node.0),
+        }
+    }
+}
+struct TabViewer<'a> {
+    added_nodes: &'a mut Vec<MyTab>,
+}
+
+impl egui_dock::TabViewer for TabViewer<'_> {
+    type Tab = MyTab;
+
+    fn title(&mut self, tab: &mut Self::Tab) -> egui_dock::egui::WidgetText {
+        tab.title().into()
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
+        tab.cartesian_tab_content(ui);
+    }
+
+    fn add_popup(&mut self, ui: &mut egui::Ui, surface: SurfaceIndex, node: NodeIndex) {
+        ui.set_min_width(80.0);
+        ui.style_mut().visuals.button_frame = false;
+
+        if ui.button("Cartesian tab").clicked() {
+            self.added_nodes.push(MyTab::cartesian(surface, node));
+        }
+    }
+}
+
+struct MyApp {
+    dock_state: DockState<MyTab>,
+    counter: usize,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        let tree = DockState::new(vec![
+            MyTab::cartesian(SurfaceIndex::main(), NodeIndex(1)),
+        ]);
+
+        Self {
+            dock_state: tree,
+            counter: 2,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let mut added_nodes = Vec::new();
+        DockArea::new(&mut self.dock_state)
+            .show_add_buttons(true)
+            .show_add_popup(true)
+            .style(egui_dock::Style::from_egui(ctx.style().as_ref()))
+            .show(
+                ctx,
+                &mut TabViewer {
+                    added_nodes: &mut added_nodes,
+                },
+            );
+        
+        added_nodes.drain(..).for_each(|node| {
+            self.dock_state
+                .set_focused_node_and_surface((node.surface, node.node));
+            self.dock_state.push_to_focused_leaf(MyTab {
+                kind: node.kind,
+                surface: node.surface,
+                node: NodeIndex(self.counter),
+                c_x: node.c_x,
+                c_y: node.c_y,
+                c_z: node.c_z,
+                t_x: node.t_x,
+                t_y: node.t_y,
+                t_z: node.t_z,
+                ammo_type: node.ammo_type,
+                charges: node.charges,
+                yaw: node.yaw,
+                pitch: node.pitch,
+                time: node.time,
+                impact_angle: node.impact_angle,
+            });
+            self.counter += 1;
+        });
+    }
 }
